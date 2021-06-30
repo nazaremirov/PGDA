ed4793a1c6bb4db1fa7466d53e484a4e675dded0
nazaremirov <69375635+nazaremirov@users.noreply.github.com>
2021-06-29 23:27:51 -0400

    Add files via upload

---
+++ PGDA_eig_vector_approx.py
@@ -0,0 +1,43 @@
+import numpy as np
+#from numpy.linalg import matrix_power
+
+def Distributed_inv_approx(H):
+    
+    N = np.size(H, axis = 0)
+    
+    row_col_max = np.maximum(np.sum(H, axis = 1), np.sum(H, axis = 0))
+    
+    local_row_col_max = np.zeros(N)
+    
+    for i in range(N):
+        for j in range(N):
+            if H[i][j]!=0:
+                local_row_col_max[i] = np.maximum(local_row_col_max[i], row_col_max[j])
+            
+    prec_matrix = np.diag(local_row_col_max)
+    
+    G = np.linalg.matrix_power(prec_matrix,-2)*H.transpose()
+    
+    return G
+
+
+def PGDA_eigvector_approx(A, eig_value, max_iter):
+    
+    N = np.size(A, axis = 0)
+    
+    H = A - eig_value*np.identity(N)
+    
+    G = Distributed_inv_approx(H)
+    
+    x = np.random.rand(N)    
+    
+    for m in range(max_iter):
+        x = np.dot(np.identity(N) - np.matmul(G, H), x)   
+    return x
+                 
+#  ############## TEST #############
+    
+# H=np.array([[7,1,0],[-1,2,0],[0,0,5]])
+
+
+# print(PGDA_eigvector_approx(H,1,400))
---
+++ PGDA_function.py
@@ -0,0 +1,58 @@
+import numpy as np
+#from numpy.linalg import matrix_power
+
+def Distributed_inv_approx(H):
+    
+    N = np.size(H,axis = 0)
+    
+    row_col_max = np.maximum(np.sum(H, axis = 1),np.sum(H, axis = 0))
+    
+    local_row_col_max = np.zeros(N)
+    
+    for i in range(N):
+        for j in range(N):
+            if H[i][j]!=0:
+                local_row_col_max[i] = np.maximum(local_row_col_max[i], row_col_max[j])
+            
+    prec_matrix = np.diag(local_row_col_max)
+    
+    G = np.linalg.matrix_power(prec_matrix,-2)*H.transpose()
+    
+    return G
+
+def Distributed_sym_inv_approx(H):
+    
+    row_col_max = np.sum(H, axis=1)
+    
+    G = np.linalg.matrix_power(np.diag(row_col_max),-1)
+    
+    return G
+
+
+def PGDA(H,y,max_iter):
+    
+    if np.allclose(H, H.T, rtol = 1e-05, atol = 1e-08):
+        G = Distributed_sym_inv_approx(H)
+    else:
+        G = Distributed_inv_approx(H)
+    
+    N = np.size(H, axis = 0)
+    
+    x = np.zeros(N)
+    e = np.zeros(N)
+    
+    for m in range(max_iter):
+        e = np.dot(H,x) - y 
+        x = x - np.dot(G,e)
+    
+    return x
+
+########### TEST ##################3                 
+  
+H=np.array([[7,1,0],[-1,2,0],[0,0,5]])
+
+f=np.array([1,2,-3])
+y=np.dot(H,f)
+
+
+print(PGDA(H,y,400))
